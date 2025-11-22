b98b68692e951a0c52d981575de2d70df62c208f
night <keanevgps3@gmail.com>
2025-11-22 22:08:19 +0200

    Add initial configuration and script files for Wazuh integration

---
+++ configs/firewallRuleAdd.ps1
@@ -0,0 +1,1 @@
+netsh advfirewall firewall add rule name="Allow ICMP" protocol=icmpv4:8,any dir=in action=allow
---
+++ scripts/testEvent.ps1
@@ -0,0 +1,1 @@
+Write-EventLog -LogName Application -Source "WazuhTest" -EntryType Error -EventId 1001 -Message "Test Wazuh log"
